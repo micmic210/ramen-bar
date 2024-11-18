020b25ccfdc21a8e6e6d821cfd518a12321839ed
Michiko Inoue <inouem888@gmail.com>
2024-11-18 22:21:39 +0000

    Style login page with Bootstrap and update button to gray color

--- ramen_bar/settings.py
+++ ramen_bar/settings.py
@@ -40,8 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms', 
+    'crispy_bootstrap5',
     'core',
 ]
+
+# Crispy forms settings
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 AUTH_USER_MODEL = 'core.CustomUser'
 
